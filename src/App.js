8684ea1a00b1fd83dba23b1a73b115f526441d2b
AnniLix <109537228+AnniLix@users.noreply.github.com>
2022-10-28 22:32:19 +0300

    Improved styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h2 className="headerName">Dictionary</h2>
+        <h2 className="headerName">Dictionary📙</h2>
         <main>
-          <Dictoinary defaultKeyword="sunset"/>
+          <Dictoinary defaultKeyword="sunset" />
         </main>
-        <footer className='app-footer'>Coded by Anna Balitska</footer>
+        <footer className="app-footer">
+          Coded by {""}
+          <a
+            href="https://www.linkedin.com/in/anna-balitska-234367242/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Anna Balitska
+          </a>
+        </footer>
       </div>
     </div>
   );
