abd81155df8427c5518b67e3b21c067a46ba4098
AnniLix <109537228+AnniLix@users.noreply.github.com>
2022-10-28 22:13:06 +0300

    Added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,34 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import Results from './Results';
+import Photos from './Photos';
 import './Dictionary.css';
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
+  }
+
   function search() {
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      '563492ad6f917000010000012e1d5ee2dd4a456bafdf7cd735495918';
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -35,9 +49,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>
-            What word do you want to look up?
-          </h1>
+          <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -49,6 +61,7 @@ export default function Dictionary(props) {
           <div className="hint">suggested words: sunset, wine, yoga...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
